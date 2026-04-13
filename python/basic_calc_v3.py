95960cf6d357d016f3b9b6eb677da734b1ce183c
damc <damc701@gmail.com>
2026-04-13 09:30:55 -0500

    calc 3

---
+++ python/basic_calc_v3.py
@@ -0,0 +1,40 @@
+import os
+
+#function main memu
+def main_menu():
+    print("### MAIN MENU ###")
+    print("[1], Addition")
+    print("[2], Sustraccion")
+    print("[3], Multiplication")
+    print("[4], DIvicion")
+    print("[5], Average")
+    print("[6], All operarions")
+
+os.system('clear')
+#inputs
+n1 = float(input("enter first number: "))
+n2 = float(input("enter second number: "))
+main_menu()
+opt = int(input("enter any option: "))
+
+if (opt == 1):
+    add = n1 + n2 
+    print(f"addition is: {add}") # print("addition is: ", add)
+
+if (opt == 2):
+    subs = n1 - n2 
+    print(f"sustraccion is: {subs}")
+
+if (opt == 3):
+    mult = n1 * n2 
+    print(f"multiplication is: {mult}")
+
+if (opt == 4):
+    div = n1 / n2 
+    print(f"divition is: {div}")
+
+if (opt == 5):
+    avg = (n1 + n2) / 2 
+    print(f"average is: {avg}")
+
+if (opt == 6):
